dc7d0cda723a9e9695886bf727d7f36289f950e0
soar_lin <soar.lin@gmail.com>
2019-12-14 02:12:38 +0800

    Update firebase.js

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,19 +1,18 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+
+const config = {
+  apiKey: $FIREBASE_APIKEY,
+  authDomain: "my-firebase-test-project-a13fb.firebaseapp.com",
+  databaseURL: "https://my-firebase-test-project-a13fb.firebaseio.com",
+  projectId: "my-firebase-test-project-a13fb",
+  storageBucket: "my-firebase-test-project-a13fb.appspot.com",
+  messagingSenderId: "1085532144000",
+  appId: "1:1085532144000:web:b5d656902d75b903940165",
+  measurementId: "G-PG2VXNZL3R"
+}
 if (!firebase.apps.length) {
-  const config = {
-    apiKey: '<replace_here>',
-    authDomain: '<replace_here>',
-    databaseURL: '<replace_here>',
-    projectId: '<replace_here>',
-    storageBucket: '<replace_here>',
-    messagingSenderId: '1<replace_here>',
-    appId: '<replace_here>',
-    measurementId: '<replace_here>'
-  }
   firebase.initializeApp(config)
-  const settings = { timestampsInSnapshots: true }
-  firebase.firestore().settings(settings)
 }
 const fireDb = firebase.firestore()
 export { fireDb }
